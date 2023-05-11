63cba8ae4d75f6384e3ac0b4660895b5d308f9e2
NazaDyA <133015241+NazaDyA@users.noreply.github.com>
2023-05-11 09:29:40 -0300

    err module import

--- dags/dag_s3.py
+++ dags/dag_s3.py
@@ -1,18 +1,12 @@
-from datetime import datetime
+import datetime
 
-from airflow import DAG
-from airflow.decorators import task
-from airflow.operators.bash import BashOperator
+from airflow.decorators import dag
+from airflow.operators.empty import EmptyOperator
 
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
 
-    # Tasks are represented as operators
-    hello = BashOperator(task_id="hello", bash_command="echo hello")
+@dag(start_date=datetime.datetime(2021, 1, 1), schedule="@daily")
+def generate_dag():
+    EmptyOperator(task_id="task")
 
-    @task()
-    def airflow():
-        print("airflow")
 
-    # Set dependencies between tasks
-    hello >> airflow()
+generate_dag()
